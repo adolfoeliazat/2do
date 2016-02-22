47090c4746e5b6f71f1064780a28c0ec95869cac
eyqs <eugeneyqshen@gmail.com>
2016-02-21 18:59:47 -0800

    Merge main.py into 2do.py, unmerge web.py

--- 2do.py
+++ 2do.py
@@ -2,17 +2,11 @@
 # Manage various desktop notifications of periodic tasks
 
 from selenium import webdriver
-import sys
 import time
+import sched
 import subprocess
-
-# Parse system arguments
-opt = ''
-val = ''
-if len(sys.argv) > 1:
-    opt = sys.argv[1]
-    if len(sys.argv) > 2:
-        val = sys.argv[2]
+OPENTIME = 30   # Time to wait for X Server to open
+STOPTIME = 300  # Time to wait for user to stop 2do
 
 # Keep dbus-send constants
 dest = 'org.freedesktop.Notifications'
@@ -25,85 +19,69 @@ actions = ''
 hints = ''
 timeout = '0'       # No timeout, everything manually dismissed
 
-
-# Choose the right messages depending on opt and val
-# No opt, that's very bad
-if not opt:
-    summary = 'Error!'
-    body = 'You called 2do with no arguments. Stop it.'
-    icon = iconpath + 'red.gif'
-
-# Time for daily tasks, called by systemd/user/2do.timer
-elif opt == '--daily':
-    time.sleep(30)  # Allow time for X server to open
-    summary = 'Keep that StackExchange streak!'
-    body = 'Automatically opening all tabs in 300 seconds.\n' + \
-           'Terminate with `killall python`.'
-    icon = iconpath + 'yellow.gif'
-
-# Time to take a break, called by systemd/user/30min.timer
-elif opt == '--break':
-    summary = 'Take a break!'
-    body = 'Rest your eyes for 5 minutes.'
-    icon = iconpath + 'orange.gif'
-
-# Laptop battery getting low, called by bin/lowbattery
-elif opt == '--lowbattery':
-    summary = 'Low battery!'
-    body = 'Battery at ' + val + '%, connect charger.'
-    if not val:
-        body = 'Battery is probably low, connect charger.'
-    icon = iconpath + 'red.gif'
-
-else:
-    summary = 'Unrecognized!'
-    body = 'You called 2do with an unrecognized argument.'
-    icon = iconpath + 'red.gif'
-
-# Send the message
-subprocess.call(['dbus-send', '--session', '--dest='+dest, path, method,
-                 'string:'+app_name, 'uint32:'+pid, 'string:'+icon,
-                 'string:'+summary, 'string:'+body, 'array:string:'+actions,
-                 'array:string:'+hints, 'int32:'+timeout])
+# Tasks to do: (period in seconds, priority, extra arguments)
+tasks = [(16, 1, '--daily'), (17, 2, '--lowbattery', '5'), (14, 3, '--break')]
+scheduler = sched.scheduler()
 
 
-# Do additional things depending on opt and val 
-# Open StackExchange sites every day
-if opt == '--daily' or opt == '--stack':
-    urls = ["http://stackoverflow.com/questions/11227809/",
-            "http://math.stackexchange.com/questions/71874/",
-            "http://physics.stackexchange.com/questions/5265/",
-            "http://music.stackexchange.com/questions/3/",
-            "http://superuser.com/questions/792607/",
-            "http://tex.stackexchange.com/questions/94889/",
-            "http://unix.stackexchange.com/questions/34196/",
-            "http://vi.stackexchange.com/questions/84/"]
+# Do a task
+def call(interval, priority, *arguments):
+    # Parse extra arguments
+    opt = ''
+    val = ''
+    if len(arguments) > 0:
+        opt = arguments[0]
+        if len(arguments) > 1:
+            val = arguments[1]
 
-    # Allow time to kill process with killall python
+    # Choose the right messages depending on opt
+    # No opt, that's very bad
+    if not opt:
+        summary = 'Error!'
+        body = 'You called 2do with no arguments. Stop it.'
+        icon = iconpath + 'red.gif'
+    # Time for daily tasks, called by systemd/user/2do.timer
+    elif opt == '--daily':
+        summary = 'Keep that StackExchange streak!'
+        body = 'Automatically opening all tabs in ' + str(STOPTIME) + \
+               ' seconds.\nTerminate with `killall python`.'
+        icon = iconpath + 'yellow.gif'
+    # Time to take a break, called by systemd/user/30min.timer
+    elif opt == '--break':
+        summary = 'Take a break!'
+        body = 'Rest your eyes for 5 minutes.'
+        icon = iconpath + 'orange.gif'
+    # Laptop battery getting low, called by bin/lowbattery
+    elif opt == '--lowbattery':
+        summary = 'Low battery!'
+        body = 'Battery at ' + val + '%, connect charger.'
+        if not val:
+            body = 'Battery is probably low, connect charger.'
+        icon = iconpath + 'red.gif'
+    else:
+        summary = 'Unrecognized!'
+        body = 'You called 2do with an unrecognized argument.'
+        icon = iconpath + 'red.gif'
+
+    # Send the message
+    subprocess.call(['dbus-send', '--session', '--dest=' + dest, path, method,
+                     'string:' + app_name, 'uint32:' + pid, 'string:' + icon,
+                     'string:'+summary,'string:'+body,'array:string:'+actions,
+                     'array:string:' + hints, 'int32:' + timeout])
+
+    # Do extra stuff
     if opt == '--daily':
-        time.sleep(300)
+        scheduler.enter(OPENTIME, 1, lambda a=['python',
+            'home/eyqs/Dropbox/Projects/2do/web.py']: subprocess.Popen(a))
 
-    # Get Gmail password from password file
-    driver = webdriver.Chrome('/usr/bin/chromedriver')
-    with open('/home/eyqs/.password', 'r') as f:
-        for line in f.readlines():
-            if line.split('; ')[0] == 'Gmail':
-                user = line.split('; ')[1] + '\n'   # Has newline so no need
-                pswd = line.split('; ')[2]          # to click sign in button
+    # Reschedule the task
+    scheduler.enter(interval, priority,
+                    lambda i=interval, p=priority, a=arguments: call(i,p,*a))
 
-    # Sign into StackExchange by logging into Gmail
-    driver.get(urls[0])
-    driver.find_element_by_xpath(
-        "//span[@class='topbar-menu-links']/a[2]").click()
-    driver.find_element_by_xpath("//div[@data-provider='google']").click()
-    username = driver.find_element_by_name('Email')
-    username.send_keys(user)
-    driver.implicitly_wait(10)  # Wait for password box to show up
-    password = driver.find_element_by_name('Passwd')
-    password.send_keys(pswd)
 
-    # Open up all StackExchange sites to get Fanatic badge
-    time.sleep(10)      # Wait for server to catch up to logins or something
-    for url in urls:
-        driver.get(url)
-    driver.close()
+if __name__ == '__main__':
+    time.sleep(OPENTIME)
+    for task in tasks:
+        scheduler.enter(task[0], task[1],
+                        lambda i=task[0], p=task[1], a=task[2:]: call(i,p,*a))
+    scheduler.run()
